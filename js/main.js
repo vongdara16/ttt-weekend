c133426606d03ec13a3a44426856409d0c63d0d6
ManliestBen <benjamintmanley@gmail.com>
2021-02-26 11:10:23 -0600

    add code

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*------Constants------*/
+
+
+
+/*------Variables (state)------*/
+
+// Variables might include (board/turn/winner)
+
+/*------Cached Element References------*/
+
+// You might choose to put your game status here
+
+/*------Event Listeners------*/
+
+// This is where you should put the event listener
+// for a mouse-click
+
+/*------Functions------*/
+
+
+// Some functions you might choose to use:
+
+// Initialization function:
+// Where you set your initial state, setting up 
+// what the board will look like upon loading
+
+// On-Click function:
+// Set up what happens when one of the elements
+// is clicked
+
+
+// Check winner function:
+// Checks the current state of the board for
+// a winner and changes the state of the winner
+// variable if so
+
+
+// Render function:
+// Displays the current state of the board
+// on the page, updating the elements to reflect
+// either X or O depending on whose turn it is
