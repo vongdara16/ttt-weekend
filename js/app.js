10ad597127e149b9a62854a016a7917526644a51
Justin Vongdara <Vongdara16@gmail.com>
2022-02-03 16:20:02 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let sqArr, turn, winner
+// winner will represent 3 different game states
+  // player won | tie | game still in play
 
 
 /*------------------------ Cached Element References ------------------------*/
