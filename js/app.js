f794d1964862f0a72143e96936702a15cce3a153
Justin Vongdara <Vongdara16@gmail.com>
2022-02-03 16:34:30 -0500

    add var and cached element refs

--- js/app.js
+++ js/app.js
@@ -3,11 +3,22 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let squArr, turn, winner
+// winner will represent 3 different game states
+  // player won | tie | game still in play
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const topL = document.querySelector('.square')
+const midL = document.querySelector('.square')
+const botL = document.querySelector('.square')
+const topM = document.querySelector('.square')
+const midM = document.querySelector('.square')
+const botM = document.querySelector('.square')
+const topR = document.querySelector('.square')
+const midR = document.querySelector('.square')
+const botR = document.querySelector('.square')
+const gameStat = document.querySelector('#message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
