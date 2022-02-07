f83408b92e88f76e4e5d45ea0569d7ec5fb5fea2
Justin Vongdara <Vongdara16@gmail.com>
2022-02-06 22:23:06 -0500

    complete ttt

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const winArray = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let squArr, turn, winner
+let squArr, turn, winner, windex
 
 
 
@@ -13,7 +13,7 @@ let squArr, turn, winner
 /*------------------------ Cached Element References ------------------------*/
 const gameStat = document.querySelector('#message')
 const board = document.querySelectorAll('.square')
-const section = document.querySelector('.board')
+const section = document.querySelector('#board')
 const btn = document.querySelector('#reset-btn')
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -36,17 +36,34 @@ function init(){
 function render(){
   squArr.forEach((elem, idx) => {
     if (elem === 1){
-      board[idx].style.backgroundColor = 'green'
-      board[idx].innerHTML = 'X'
+      playerX(idx)
     } else if (elem === -1){
-      board[idx].style.backgroundColor = 'red'
-      board[idx].innerHTML = 'O'
+      playerO(idx)
     } else { 
       board[idx].style.backgroundColor = ''
+      board[idx].style.opacity = ''
       board[idx].innerHTML = ''
     }
   })
   renderMsg()
+
+  if (winner !== null && winner !== 'T'){
+    confetti.start(3000);
+  }
+}
+
+function playerX(idx){
+  board[idx].style.backgroundColor = 'white'
+  board[idx].style.opacity = 0.7
+  board[idx].style.color = 'blue'
+  board[idx].innerHTML = 'X'
+}
+
+function playerO(idx){
+  board[idx].style.backgroundColor = 'white'
+  board[idx].style.opacity = 0.7
+  board[idx].style.color = 'red'
+  board[idx].innerHTML = 'O'
 }
 
 function renderMsg(){
@@ -78,6 +95,7 @@ function handleClick(evt){
   getWinner()
   getTie()
   render()
+  winColor(windex)
 }
 
 
@@ -86,10 +104,18 @@ function getWinner(){
     let totalCheck = Math.abs(squArr[combo[0]] + squArr[combo[1]] + squArr[combo[2]])
     if (totalCheck === 3){
       winner = turn*-1
+      windex = [combo[0], combo[1], combo[2]]
       return
     }
   })
+}
 
+function winColor(windex){
+  if (winner !== null && winner !== 'T'){
+    board[windex[0]].style.backgroundColor = 'limegreen'
+    board[windex[1]].style.backgroundColor = 'limegreen'
+    board[windex[2]].style.backgroundColor = 'limegreen'
+  }
 }
 
 function getTie(){
